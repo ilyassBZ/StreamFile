db947271b1ff77f45762b9145d1a8f800945a569
ilyassBZ <bzitarilyass75@gmail.com>
2024-02-03 16:21:32 +0100

    update(search): i did make the srv send rslt by 16 byte  to the client

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -21,41 +21,65 @@ const search = async (req, res) => {
         },
       },
     });
+    // const filePath = "search_results.txt";
+    //
+    // const pat = path.join(__dirname, `../${filePath}`);
+    // const fileStream = fs.createWriteStream(filePath);
+    //
+    // result.forEach((result) => {
+    //   fileStream.write(`${result.name}\n${JSON.stringify(result.data)}\n\n`);
+    // });
+    //
+    // fileStream.end();
+    //
+    // fileStream.on("finish", () => {
+    //   const readStream = fs.createReadStream(pat);
+    //   res.setHeader(
+    //     "Content-disposition",
+    //     `attachment; filename=search_results.txt`,
+    //   );
+    //   res.setHeader("Content-type", "text/html");
+    //   readStream.pipe(res);
+    //
+    //   readStream.on("close", () => {
+    //     console.log("File stream closed.");
+    //     // fs.unlink(filePath, (err) => {
+    //     //   if (err) {
+    //     //     console.error("Error deleting file:", err);
+    //     //   } else {
+    //     //     console.log("File deleted successfully.");
+    //     //   }
+    //     // });
+    //   });
+    // });
 
-    const filePath = "search_results.txt";
+    //trying to  make this even fast iim gona stream the res derectly to the user without making file in the system
 
-    const pat = path.join(__dirname, `../${filePath}`);
-    const fileStream = fs.createWriteStream(filePath);
+    const textData = result
+      .map((entry) => `${entry.name}\n${JSON.stringify(entry.data)}\n\n`)
+      .join("");
 
-    result.forEach((result) => {
-      fileStream.write(`${result.name}\n${JSON.stringify(result.data)}\n\n`);
+    res.setHeader("Content-disposition", "attachment; filename=file.txt");
+    res.setHeader("Content-type", "text/plain");
+    const chunkSize = 16;
+    const readStream = Readable.from(textData, { highWaterMark: chunkSize });
+
+    readStream.pipe(res);
+
+    readStream.on("end", () => {
+      console.log("File stream ended.");
     });
 
-    fileStream.end();
-
-    fileStream.on("finish", () => {
-      const readStream = fs.createReadStream(pat);
-      res.setHeader(
-        "Content-disposition",
-        `attachment; filename=search_results.txt`,
-      );
-      res.setHeader("Content-type", "text/html");
-      readStream.pipe(res);
-
-      readStream.on("close", () => {
-        console.log("File stream closed.");
-        // fs.unlink(filePath, (err) => {
-        //   if (err) {
-        //     console.error("Error deleting file:", err);
-        //   } else {
-        //     console.log("File deleted successfully.");
-        //   }
-        // });
-      });
+    readStream.on("close", () => {
+      console.log("File stream closed.");
+    });
+    readStream.on("error", (err) => {
+      console.error(err);
+      res.status(500).end("server err");
     });
   } catch (error) {
     console.error("Error in search:", error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: " server error" });
   }
 };
 
